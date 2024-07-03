165fd758e46eabde07d5a6f1f61efe431ae5d5f5
Apocalypseong <94913214+Apocalypse3007@users.noreply.github.com>
2024-07-03 17:32:31 +0530

    .

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import { Hono } from 'hono'
+import { userRouter } from './Routes/user'; // Fix the casing here
+import { blogRouter } from './Routes/blog';
+import { cors } from 'hono/cors'
+
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  }
+}>();
+
+app.use('/*', cors())
+app.route("/api/v1/user", userRouter);
+app.route("/api/v1/blog", blogRouter);
+
+export default app
